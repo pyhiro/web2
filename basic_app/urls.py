80d775d65f65c8a2eabf42d01287f79986a43f6d
pyhiro <thwkana@gmail.com>
2020-04-28 15:48:31 +0900

    first commit

---
+++ basic_app/urls.py
@@ -0,0 +1,9 @@
+from basic_app import views
+from django.urls import path
+app_name = 'basic_app'
+
+urlpatterns = [
+    path("relative/", views.relative, name="relative"),
+    path("other/", views.other, name="other")
+]
+
